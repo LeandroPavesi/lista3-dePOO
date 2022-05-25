ea3bfcfe018d9cce355c07a58c2134f7078f0064
LeandroPavesi <86697123+LeandroPavesi@users.noreply.github.com>
2022-05-25 20:42:00 -0300

    Add files via upload

--- L3Q8.py
+++ L3Q8.py
@@ -1,6 +1,6 @@
 from cmath import sqrt
 
-with open('questao 8/usuarios.csv', 'r', encoding='utf-8') as arquivo:
+with open('usuarios.csv', 'r', encoding='utf-8') as arquivo:
     conteudo = arquivo.read()
 
 linhas = conteudo.split('\n')
@@ -19,43 +19,38 @@ for linha in linhas:
             espaço.append(fragmentos[1])
             usuarios2.append(fragmentos[0])
 
-print(usuarios)
 #transformando a lista com os dados de consumo em inteiros
 novo_espaço = list(map(int, espaço))
-print(novo_espaço)
+
 
 #convertendo bytes para MegaBytes
 convertido = []
 for converter in novo_espaço:
     mb = (converter/(1024**2))
     convertido.append(mb)
-print(convertido)
 
+#total de consumo
 total_bytes = sum(convertido)
-print(total_bytes)
+
 
 usuarios_str = list(map(str, usuarios2))
 consumo_str = list(map(str, convertido))
 
 juntos = []
 
-print('Juntos', usuarios2)
-
-with open('questao 8/relatorio.txt', "w") as arqW:
+with open('relatorio.txt', "w") as arqW:
     arqW.write('Usuario Espaco utilizado    % do uso')
 
     usado = []
 
     for uso in convertido:
         usado.append(((uso*100)/total_bytes))
-
-    print('Usado>', usado)
     
-    for i in usuarios2:
-        for c in convertido:
-            for d in usado:
-                arqW.write(f'\n{i}    {c:.2f}   {d:.2f}\n ')
-            
+    for i in range(len(usuarios2)):
+        arqW.write(f'\n{usuarios2[i]} {convertido[i]:.5}MB {usado[i]:.5}%\n')
+        
+    arqW.write(f'\nEspaco total ocupado: {total_bytes:.5}MB')
+    arqW.write(f'\nEspaco medio ocupado: {(total_bytes/6):.5}MB')
             
             
 
